7e9623f517131b39a7481477f09ef02e8679df99
Bartlomiej Kozal <bartlomiejkozal@gmail.com>
2010-06-04 13:24:07 +0200

    Added main function

--- main.cpp
+++ main.cpp
@@ -11,3 +11,14 @@
 #include <GLUT/GLUT.h>
 #include <OpenCL/OpenCL.h>
 
+int main(int argc, char *argv[]) {
+	glutInit(&argc, argv);
+	glutInitWindowSize(800, 800);
+	glutInitWindowPosition(300, 300);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+	glutCreateWindow("Cottages");
+//	glutReshapeFunc(reshape);
+//	glutDisplayFunc(draw);
+	glutMainLoop();
+	return 0;
+}
