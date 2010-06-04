22b9b47d8c8176952947734e4e1dde55257cd303
Bartlomiej Kozal <bartlomiejkozal@gmail.com>
2010-06-04 13:12:02 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/*
+ *  main.cpp
+ *  cottages
+ *
+ *  Created by Bartlomiej Kozal on 04/06/2010.
+ *  Copyright 2010 Bartlomiej Kozal. All rights reserved.
+ *
+ */
+
+
+#include <GLUT/GLUT.h>
+#include <OpenCL/OpenCL.h>
+
